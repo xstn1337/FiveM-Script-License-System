5e6e9299cd1ea7953235dcb92d9e7485fcd3399b
xstn1337 <74708959+xstn1337@users.noreply.github.com>
2021-04-26 20:45:42 +0200

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
     $whitelist = array('127.0.0.1, '0.0.0.0');
     if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)) {
-        echo 'WoW! you have access';
+        echo 'access';
     } else {
         http_response_code(404);
     }
